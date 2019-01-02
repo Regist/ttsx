d749e7d9d7c174b2abac731cbef949f5a585a37c
just4regist <just4regist@qq.com>
2019-01-02 21:58:54 +0800

    展示页面下部数据

--- controllers/goods.go
+++ controllers/goods.go
@@ -33,6 +33,29 @@ func (this *GoodsController) ShowIndex() {
 	var indexPromotionBanners []models.IndexPromotionBanner
 	ormer.QueryTable("IndexPromotionBanner").OrderBy("Index").All(&indexPromotionBanners)
 
+	// 查询下部的商品
+	var goodsSkus = make([]map[string]interface{}, len(goodTypes))
+
+	// 查询商品类型
+	for index, _ := range goodsSkus {
+		temp := make(map[string]interface{})
+		temp["types"] = goodTypes[index]
+		goodsSkus[index] = temp
+	}
+
+	// 查询商品
+	for _, goodsMap := range goodsSkus {
+		var goodsImage []models.IndexTypeGoodsBanner
+		var goodsText []models.IndexTypeGoodsBanner
+		ormer.QueryTable("IndexTypeGoodsBanner").RelatedSel("GoodsType", "GoodsSKU").Filter("GoodsType", goodsMap["types"]).Filter("DisplayType", 1).All(&goodsImage)
+		ormer.QueryTable("IndexTypeGoodsBanner").RelatedSel("GoodsType", "GoodsSKU").Filter("GoodsType", goodsMap["types"]).Filter("DisplayType", 0).All(&goodsText)
+
+		goodsMap["goodsImage"] = goodsImage
+		goodsMap["goodsText"] = goodsText
+	}
+
+	this.Data["goodsSkus"] = goodsSkus
+
 	this.Data["goodTypes"] = goodTypes
 	this.Data["indexGoodsBanners"] = indexGoodsBanners
 	this.Data["indexPromotionBanners"] = indexPromotionBanners
