7ce72ac5abc9069fc8a56267f18d67ae98d5df27
just4regist <just4regist@qq.com>
2019-01-02 16:30:23 +0800

    展示页面顶部数据

--- controllers/goods.go
+++ controllers/goods.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"ttsx/models"
+)
 
 type GoodsController struct {
 	beego.Controller
@@ -15,6 +19,23 @@ func (this *GoodsController) ShowIndex() {
 	} else {
 		this.Data["username"] = username.(string)
 	}
+
+	ormer := orm.NewOrm()
+	// 查询主页分类数据
+	var goodTypes []models.GoodsType
+	ormer.QueryTable("GoodsType").All(&goodTypes)
+
+	// 查询主页轮播图
+	var indexGoodsBanners []models.IndexGoodsBanner
+	ormer.QueryTable("IndexGoodsBanner").OrderBy("Index").All(&indexGoodsBanners)
+
+	// 查询主页推广
+	var indexPromotionBanners []models.IndexPromotionBanner
+	ormer.QueryTable("IndexPromotionBanner").OrderBy("Index").All(&indexPromotionBanners)
+
+	this.Data["goodTypes"] = goodTypes
+	this.Data["indexGoodsBanners"] = indexGoodsBanners
+	this.Data["indexPromotionBanners"] = indexPromotionBanners
 
 	this.TplName = "index.html"
 }
